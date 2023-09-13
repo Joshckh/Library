3e571005481b81e11f837bda12374aeee33358fa
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-09-13 23:02:56 +0800

    Added basic buttons and a form

--- js.js
+++ js.js
@@ -9,46 +9,27 @@ function Book(title,author,pages,read) {
 
 
 function addBook(book){
-    myLibrary.push()
+    myLibrary.push(book)
 }
 
-.card {
-    width: 190px;
-    background: white;
-    padding: .4em;
-    border-radius: 6px;
-  }
-  
-  .card-image {
-    background-color: rgb(236, 236, 236);
-    width: 100%;
-    height: 130px;
-    border-radius: 6px 6px 0 0;
-  }
-  
-  .card-image:hover {
-    transform: scale(0.98);
-  }
-  
-  .category {
-    text-transform: uppercase;
-    font-size: 0.7em;
-    font-weight: 600;
-    color: rgb(63, 121, 230);
-    padding: 10px 7px 0;
-  }
-  
-  .category:hover {
-    cursor: pointer;
-  }
-  
-  .heading {
-    font-weight: 600;
-    color: rgb(88, 87, 87);
-    padding: 7px;
-  }
-  
-  .heading:hover {
-    cursor: pointer;
-  }
-  
+
+const formCont = document.querySelector(".formCont")
+const addBookButton = document.querySelector(".addBook") 
+
+addBookButton.addEventListener("click",() =>{
+  formCont.style.display="grid"
+})
+
+/*
+let book1 = {
+  title:"The Hobbit",
+  author:"tolken",
+  pages:365,
+  read:true
+}
+
+addBook(book1)
+
+console.log(myLibrary)
+
+*/
