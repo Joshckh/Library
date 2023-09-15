68063a028fe171f147aac0ea48fb806f94b637a1
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-09-15 16:33:19 +0800

    Added Js and css for the form

--- js.js
+++ js.js
@@ -1,6 +1,15 @@
+const formCont = document.querySelector(".formCont")
+const addBookButton = document.querySelector(".addBook") 
+const add = document.querySelector("#add")
+const inputs = document.querySelectorAll("input")
+const container = document.querySelector(".container")
+
+
 const myLibrary = [];
 
-function Book(title,author,pages,read) {
+
+function Book(image, title,author,pages,read) {
+    this.image = createImageBitmap
     this.title = title
     this.author = author
     this.pages = pages
@@ -12,13 +21,45 @@ function addBook(book){
     myLibrary.push(book)
 }
 
+console.log
 
-const formCont = document.querySelector(".formCont")
-const addBookButton = document.querySelector(".addBook") 
+add.addEventListener("click", () => {
+  const image = document.querySelector("#image").files[0]; 
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = parseInt(document.querySelector("#pages").value);
+  const read = document.querySelectorAll("[name='read']").checked;
+
+
+  let book = new Book(image, title, author, pages, read);
+
+  
+  addBook(book);
+
+ 
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach(input => {
+      if (input.type === "checkbox" || input.type === "radio") {
+          input.checked = false; 
+      } else if (input.type === "file") {
+          input.value = ""; 
+      } else {
+          input.value = ""; 
+      }
+  });
+
+
+  formCont.style.display = "none";
+});
+
+
 
 addBookButton.addEventListener("click",() =>{
-  formCont.style.display="grid"
+  container.style.filter = "blur(5px)";
+  formCont.style.display="grid";
+//
 })
+
 
 /*
 let book1 = {
