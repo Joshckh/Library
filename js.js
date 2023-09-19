5ed957fbd037f82c2f0ec2beabca62fbfe211d25
Joshckh <105426491+Joshckh@users.noreply.github.com>
2023-09-19 13:24:50 +0800

    Added a button to remove books in the library

--- js.js
+++ js.js
@@ -4,6 +4,9 @@ const addBookButton = document.querySelector(".addBook");
 const add = document.querySelector("#add");
 const inputs = document.querySelectorAll("input");
 const container = document.querySelector(".container");
+const removeButtons = document.querySelectorAll(".deleteButton");
+const card = document.querySelector(".card");
+const deleteButtons = document.querySelectorAll(".deleteButton");
 
 // Create an array to store books
 const myLibrary = [];
@@ -21,6 +24,24 @@ function Book(image, title, author, pages, read) {
 function addBook(book) {
   myLibrary.push(book);
 }
+
+function delBook(title) {
+  // Ask for confirmation
+  const confirmed = confirm(`Are you sure you want to remove the book "${title}"?`);
+
+  if (confirmed) {
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (title === myLibrary[i].title) {
+        myLibrary.splice(i, 1); // Remove the book with the matching title
+        break; // Exit the loop after the book is deleted
+      }
+    }
+
+    // Update the library display after removing the book
+    updateLibrary();
+  }
+}
+
 
 // Function to update the library display
 function updateLibrary() {
@@ -57,14 +78,27 @@ function updateLibrary() {
     pages.classList.add("pages");
     pages.textContent = `${book.pages} pages`;
 
+    const delButton = document.createElement("button")
+    delButton.classList.add("deleteButton")
+    delButton.textContent = "Remove"
+    delButton.addEventListener("click", (event) => {
+      // Get the book title associated with the clicked button
+      const bookTitle = event.target.parentElement.querySelector(".title").textContent;
+  
+      // Call the delBook function to remove the book by title
+      delBook(bookTitle);
+    });
+  
     bookCard.appendChild(bookImage);
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
+    bookCard.appendChild(delButton)
 
     booksContainer.appendChild(bookCard);
   });
 }
+
 
 // Event listener for the "Add" button click
 add.addEventListener("click", () => {
